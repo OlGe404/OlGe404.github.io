1c4e4572547f57b8a3b6c6cb1a4f78559d35c51d
OlGe404 <noneOfYourBusiness@hotmail.de>
2025-01-07 07:51:43 +0100

    mv share-buttons.html into recipes.html

--- assets/js/copyUrl.js
+++ assets/js/copyUrl.js
@@ -1,6 +1,6 @@
 document.getElementById("copy-url").addEventListener("click", function(e) {
     e.preventDefault();
     navigator.clipboard.writeText(window.location.href).then(() => {
-        alert("URL copied to clipboard!");
+        alert("URL wurde in die Zwischenablage kopiert");
     });
 });
