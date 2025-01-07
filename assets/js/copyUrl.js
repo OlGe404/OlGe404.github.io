000efb0b8f6effc1dcef0dd0f8e4a1daa60f91d4
OlGe404 <noneOfYourBusiness@hotmail.de>
2025-01-07 08:09:44 +0100

    update recipes layout

--- assets/js/copyUrl.js
+++ assets/js/copyUrl.js
@@ -1,6 +1,6 @@
 document.getElementById("copy-url").addEventListener("click", function(e) {
     e.preventDefault();
     navigator.clipboard.writeText(window.location.href).then(() => {
-        alert("URL wurde in die Zwischenablage kopiert");
+        alert("URL wurde in die Zwischenablage kopiert!");
     });
 });
